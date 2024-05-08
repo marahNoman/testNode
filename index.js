87cd960195be664bc128994a1cc9f9a928d7f711
marahNoman <marah@mapletele.com>
2024-05-08 16:24:06 +0300

    open whatsapp

--- index.js
+++ index.js
@@ -205,7 +205,13 @@ try{
             if (!activated) {
                 console.log("\n * No avd activated, Starting init script. \n");
                 exec(`${ADB} -s emulator-5164 shell input keyevent KEYCODE_HOME`); 
-                console.log(exec(`${ADB} -s emulator-5164 shell input keyevent KEYCODE_HOME`));
+                await new Promise(resolve => setTimeout(resolve, 1500));
+                var startApp =exec(`${ADB} -s emulator-${this.emulatorPort} shell am start -n com.whatsapp/.Main`);
+                await new Promise((resolve, reject)=>{
+                  startApp.on('close',(code)=>{
+                      resolve();
+                  });
+                });
                 return;
                 //TODO update saby data to activated false and activationStatus Not Active
             } else {
